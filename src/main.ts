16b4702858ab7c4b7b2569383eb0e5df7248dd0c
ra0321 <alphanetra0321@gmail.com>
2024-04-18 20:37:42 +0200

    upgrade

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 const validationPipeService = require('@pipets/validation-pipes');
 
 async function bootstrap() {
@@ -8,6 +9,18 @@ async function bootstrap() {
     validationPipeService();
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
+
+    // Swagger Options
+    const options = new DocumentBuilder()
+      .addBearerAuth()
+      .setTitle('Swagger API')
+      .setDescription('Swagger API description')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, options);
+    // Swagger path: http://localhost:3000/docs
+    SwaggerModule.setup(`docs`, app, document);
+
     await app.listen(3000);
     console.log(`Application is running on: ${await app.getUrl()}`);
   } catch(err) {
